7702108abd22872e80e66e5c495cdd06aa86e944
hasan <bitbeter@gmail.com>
2024-03-20 14:43:17 +0330

    chore: add config server for cloud services

---
+++ api-gateway/src/main/java/ir/mah/apigateway/ConfigClient.java
@@ -0,0 +1,26 @@
+package ir.mah.apigateway;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigClient {
+
+    @Value("${title:Title}")
+    private String role;
+
+    @Value("${testing-key:TestKey}")
+    private String testingKey;
+
+    @GetMapping(
+            value = "/whoami/{username}",
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    public String whoami(@PathVariable("username") String username) {
+        return String.format("Hello! You 're %s, [title: %s, testing-key: %s]", username, role, testingKey);
+    }
+}
